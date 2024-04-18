08cf914f65c091c80792d7b32e5abb675feaba82
ChooseImage <chooseimage0125@gmail.com>
2024-04-18 18:08:40 +0800

    color shifting

--- Weather/weather-04/sketch.js
+++ Weather/weather-04/sketch.js
@@ -1,12 +1,16 @@
 let url = "https://coolors.co/fe9920-1e91d6-d87cac-f9b9c3-341b74";
 let pallete;
 let graphics;
+let angles;
+let xNum = 3;
+let yNum = 1;
 
 function setup() {
   createCanvas(800, 600);
   colorMode(HSB, 360, 100, 100, 100);
   angleMode(DEGREES);
   print("url: ", url);
+  angles = generate2DArray(xNum, yNum);
 
   graphics = createGraphics(width, height);
   graphics.colorMode(HSB, 360, 100, 100, 100);
@@ -25,15 +29,14 @@ function draw() {
   frameRate(0);
   background(0, 0, 90);
   pallete = createPallete(url);
-  console.log("pallet:", pallete);
 
   let offset = 20;
   let margin = 0;
 
   let cellsX = int(random(4, 12));
   let cellsY = int(cellsX * 0.75);
-  cellsX = 4;
-  cellsY = 3;
+  cellsX = xNum;
+  cellsY = yNum;
   let d = (width - offset * 2 - margin * (cellsX - 1)) / cellsX;
 
   for (let j = 0; j < cellsY; j++) {
@@ -42,15 +45,15 @@ function draw() {
       let y = offset + j * (d + margin) + d / 2;
       push();
       translate(x, y);
-      rotate((int(random(4)) * 360) / 4);
-      drawGradientArc(-d / 2, -d / 2, d * 2, 0, 90, pallete.concat());
+      rotate(angles[j][i]);
+      drawGradientArc(-d / 2, -d / 2, d * 2, 0, 90, pallete.concat(), i, j);
       pop();
     }
   }
   image(graphics, 0, 0);
 }
 
-function drawGradientArc(_x, _y, _d, angleA, angleB, colors) {
+function drawGradientArc(_x, _y, _d, angleA, angleB, colors, i, j) {
   console.log(colors);
   push();
   translate(_x, _y);
@@ -58,10 +61,14 @@ function drawGradientArc(_x, _y, _d, angleA, angleB, colors) {
   let angleMax = max(angleA, angleB);
   let cNum = int(random(colors.length));
   let c = colors[cNum];
+  colorMode(RGB);
   colors.splice(cNum, 1);
 
-  let cANum = int(random(colors.length));
-  let cA = colors[cANum];
+  let cANum = int(noise(colors.length));
+  console.log("cANum:", cANum);
+  let Red = noise(millis()) * 0.002 * 255;
+  let cA = (Red, 40, 30);
+  console.log("cA:", cA);
   colors.splice(cANum, 1);
 
   let cBNum = int(random(colors.length));
@@ -70,8 +77,12 @@ function drawGradientArc(_x, _y, _d, angleA, angleB, colors) {
   for (let angle = angleMin; angle <= angleMax; angle += 0.3) {
     let x = (cos(angle) * _d) / 2;
     let y = (sin(angle) * _d) / 2;
-    colorMode(RGB);
-    let cc = lerpColor(color(cA), color(cB), angle / abs(angleMax - angleMin));
+    colorMode(HSB);
+    let cc = lerpColor(
+      color(120 + noise(millis() * 0.0001) * 360, 30, 80),
+      color(-60 + noise(i, millis() * 0.0001) * 360, 90, 30),
+      angle / abs(angleMax - angleMin)
+    );
     stroke(cc);
     strokeWeight(2);
     line(x, y, 0, 0);
@@ -88,4 +99,22 @@ function createPallete(_url) {
   }
   console.log(arr);
   return arr;
+}
+
+function generate2DArray(x, y) {
+  const possibleValues = [0, 90, 180, 270]; // Possible values for the array elements
+  const outerArray = [];
+  const numRows = y; // Number of sub-arrays
+  const numCols = x; // Number of items in each sub-array
+
+  for (let i = 0; i < numRows; i++) {
+    const innerArray = [];
+    for (let j = 0; j < numCols; j++) {
+      const randomIndex = Math.floor(Math.random() * possibleValues.length); // Get a random index
+      innerArray.push(possibleValues[randomIndex]); // Add the value at the random index to the sub-array
+    }
+    outerArray.push(innerArray); // Add the sub-array to the outer array
+  }
+
+  return outerArray;
 }
