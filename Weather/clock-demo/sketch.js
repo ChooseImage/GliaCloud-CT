53fd66c1968b9bdeb40e3fdec263f0639a657c1c
ChooseImage <chooseimage0125@gmail.com>
2024-07-16 15:11:27 +0800

    init clock animation

--- Weather/clock-demo/sketch.js
+++ Weather/clock-demo/sketch.js
@@ -103,6 +103,9 @@ const PARAMS = {
   clockSize: 100,
   clockFade: 1,
   minute: 0,
+  animate: true,
+  clockSpeed: 25,
+  easingStrength: 2,
 };
 var cloud;
 let forecast = [21, 25, 0.7]; // time, temp, humidity;
@@ -165,7 +168,6 @@ async function setup() {
   //coldColor = color(188, 217, 215, 0);
 
   PARAMS.minute = minute();
-
   createCanvas(_Width, _Height);
 
   glassShader = createFilterShader(`
@@ -266,6 +268,9 @@ function draw() {
   }
   // drawText(textGraphic);
   //image(cloudIcon, 500, 270, 90, 56);
+  if (PARAMS.animate) {
+    animate();
+  }
 }
 
 // -------------------- UTILS -------------------------------
@@ -764,7 +769,7 @@ const setupDebugPanel = () => {
   });
   misc.addInput(PARAMS, "clockSize", {
     min: 0,
-    max: 400,
+    max: 4000,
   });
   misc.addInput(PARAMS, "clockFade", {
     min: 0.0001,
@@ -774,173 +779,18 @@ const setupDebugPanel = () => {
     min: 0,
     max: 60,
   });
+  misc.addInput(PARAMS, "animate", {
+    view: "checkbox",
+  });
+  misc.addInput(PARAMS, "clockSpeed", {
+    min: 0,
+    max: 60,
+  });
+  misc.addInput(PARAMS, "easingStrength", {
+    min: 0,
+    max: 10,
+  });
 };
-
-class Easing {
-  static easeInSine(x) {
-    return 1 - Math.cos((x * Math.PI) / 2);
-  }
-
-  static easeOutSine(x) {
-    return Math.sin((x * Math.PI) / 2);
-  }
-
-  static easeInOutSine(x) {
-    return -(Math.cos(Math.PI * x) - 1) / 2;
-  }
-
-  static easeInQuad(x) {
-    return x * x;
-  }
-
-  static easeOutQuad(x) {
-    return 1 - (1 - x) * (1 - x);
-  }
-
-  static easeInOutQuad(x) {
-    return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
-  }
-
-  static easeInCubic(x) {
-    return x * x * x;
-  }
-
-  static easeOutCubic(x) {
-    return 1 - Math.pow(1 - x, 3);
-  }
-
-  static easeInOutCubic(x) {
-    return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
-  }
-
-  static easeInQuart(x) {
-    return x * x * x * x;
-  }
-
-  static easeOutQuart(x) {
-    return 1 - Math.pow(1 - x, 4);
-  }
-
-  static easeInOutQuart(x) {
-    return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
-  }
-
-  static easeInQuint(x) {
-    return x * x * x * x * x;
-  }
-
-  static easeOutQuint(x) {
-    return 1 - Math.pow(1 - x, 5);
-  }
-
-  static easeInOutQuint(x) {
-    return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
-  }
-
-  static easeInExpo(x) {
-    return x === 0 ? 0 : Math.pow(2, 10 * x - 10);
-  }
-
-  static easeOutExpo(x) {
-    return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
-  }
-
-  static easeInOutExpo(x) {
-    return x === 0
-      ? 0
-      : x === 1
-      ? 1
-      : x < 0.5
-      ? Math.pow(2, 20 * x - 10) / 2
-      : (2 - Math.pow(2, -20 * x + 10)) / 2;
-  }
-
-  static easeInCirc(x) {
-    return 1 - Math.sqrt(1 - Math.pow(x, 2));
-  }
-
-  static easeOutCirc(x) {
-    return Math.sqrt(1 - Math.pow(x - 1, 2));
-  }
-
-  static easeInOutCirc(x) {
-    return x < 0.5
-      ? (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2
-      : (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
-  }
-
-  static easeInBack(x) {
-    const c1 = 1.70158;
-    const c3 = c1 + 1;
-    return c3 * x * x * x - c1 * x * x;
-  }
-
-  static easeOutBack(x) {
-    const c1 = 1.70158;
-    const c3 = c1 + 1;
-    return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
-  }
-
-  static easeInOutBack(x) {
-    const c1 = 1.70158;
-    const c2 = c1 * 1.525;
-    return x < 0.5
-      ? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
-      : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
-  }
-  static easeInElastic(x) {
-    const c4 = (2 * Math.PI) / 3;
-    return x === 0
-      ? 0
-      : x === 1
-      ? 1
-      : -Math.pow(2, 10 * x - 10) * Math.sin((x * 10 - 10.75) * c4);
-  }
-
-  static easeOutElastic(x) {
-    const c4 = (2 * Math.PI) / 3;
-    return x === 0
-      ? 0
-      : x === 1
-      ? 1
-      : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
-  }
-
-  static easeInOutElastic(x) {
-    const c5 = (2 * Math.PI) / 4.5;
-    return x === 0
-      ? 0
-      : x === 1
-      ? 1
-      : x < 0.5
-      ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
-      : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
-  }
-
-  static easeInBounce(x) {
-    return 1 - Easing.easeOutBounce(1 - x);
-  }
-
-  static easeOutBounce(x) {
-    const n1 = 7.5625;
-    const d1 = 2.75;
-    if (x < 1 / d1) {
-      return n1 * x * x;
-    } else if (x < 2 / d1) {
-      return n1 * (x -= 1.5 / d1) * x + 0.75;
-    } else if (x < 2.5 / d1) {
-      return n1 * (x -= 2.25 / d1) * x + 0.9375;
-    } else {
-      return n1 * (x -= 2.625 / d1) * x + 0.984375;
-    }
-  }
-
-  static easeInOutBounce(x) {
-    return x < 0.5
-      ? (1 - Easing.easeOutBounce(1 - 2 * x)) / 2
-      : (1 + Easing.easeOutBounce(2 * x - 1)) / 2;
-  }
-}
 
 // ref https://github.com/Creativeguru97/YouTube_tutorial/blob/master/p5_hacks/Gradient_effect/conical_gradient/sketch.js
 function conicGradient(sA, sX, sY, colors) {
@@ -1015,4 +865,25 @@ function conicCircle(x, y, r, colors, angle) {
   ctx.fillStyle = gradient;
   ellipse(x, y, r, r);
   blendMode(BLEND);
+}
+
+function animate() {
+  const timeLine = millis();
+  animateClock(timeLine, PARAMS.clockSpeed);
+}
+
+function animateClock(timeLine, speed = 1) {
+  const loopDuration = 60000; // 60 seconds in milliseconds
+  const totalProgress = (timeLine * speed) / loopDuration;
+  const currentLoop = Math.floor(totalProgress);
+  const loopProgress = totalProgress - currentLoop;
+  const easeInOutProgress = easeInOutQuad(loopProgress, PARAMS.easingStrength);
+  PARAMS.minute = map(easeInOutProgress, 0, 1, 0, 60);
+  PARAMS.clockFade = easeInOutProgress;
+}
+
+function easeInOutQuad(t, strength = 2) {
+  return t < 0.5
+    ? Math.pow(2 * t, strength) / 2
+    : 1 - Math.pow(2 * (1 - t), strength) / 2;
 }
